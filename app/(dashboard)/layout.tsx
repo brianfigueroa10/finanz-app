c3d93d40fd02527903af2a0340aac4f334ebde70
BrianFigueroa <bryan09figueroa@gmail.com>
2024-08-05 12:34:20 -0300

    correccion width layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,31 +1,25 @@
 'use client'
 
-import "./../globals.css"
-import React from "react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import NavbarUser from "@/components/userComponents/NavbarUser"
+import './../globals.css'
+import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import NavbarUser from '@/components/userComponents/NavbarUser'
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode
-  }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient({}))
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex flex-col container min-h-screen" >
+      <div className="flex flex-col min-h-screen">
         <header className="flex-auto">
-        <NavbarUser />
+          <NavbarUser />
         </header>
-        <main className="flex flex-col flex-1 " >
-      {children}
-      </main>
-        <footer className="border-t w-screen flex  items-center justify-center flex-auto h-24 max-h-24">
+        <main className="flex flex-col flex-1 ">{children}</main>
+        <footer className="border-t w-full flex  items-center justify-center flex-auto h-24 max-h-24">
           <p className="font-bold">Copyright © 2024</p>
         </footer>
       </div>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
